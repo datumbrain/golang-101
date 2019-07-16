67873b700db429efe4f32242587109a867d2ec45
Fahad <fsdqui@gmail.com>
2019-07-16 17:13:49 +0500

    First go program.

---
+++ 001-hello.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+	var z int = 10
+
+	fmt.Println(z)
+	n := 10
+	for i := 0; i < n; i++ {
+		fmt.Print(i)
+		if i != n-1 {
+			fmt.Print(", ")
+		} else {
+			fmt.Println()
+		}
+	}
+
+	foo() //
+}
+
+func foo() {
+	fmt.Println("This is my first (second, actually) function.")
+}
